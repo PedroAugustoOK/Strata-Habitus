53a9d6cf9183cd6e06490740df843a529550c69c
ankh <pedro13marchetti@pm.me>
2026-03-20 00:41:06 -0400

    fix: deprecations nixpkgs-unstable

--- configuration.nix
+++ configuration.nix
@@ -107,8 +107,8 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-      user    = "greeter";
+    command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+    user    = "greeter";
     };
   };
 

b624553b91b1b53166717d0865ab787f8d725b03
ankh <pedro13marchetti@pm.me>
2026-03-19 23:17:29 -0400

    add: stylix

--- flake.nix
+++ flake.nix
@@ -2,24 +2,32 @@
   description = "dotfiles do ankh";
 
   inputs = {
-   nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-   home-manager = {
-    url = "github:nix-community/home-manager";
-    inputs.nixpkgs.follows = "nixpkgs";
-   };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
-   nixosConfigurations.galaxybook = nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
-    modules = [
-     ./configuration.nix
-     home-manager.nixosModules.home-manager {
-     home-manager.useGlobalPkgs    = true;
-     home-manager.useUserPackages  = true;
-     home-manager.users.ankh       = import ./home.nix;
-    }
-   ];
+  outputs = { self, nixpkgs, home-manager, stylix, ... }: {
+    nixosConfigurations.galaxybook = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        stylix.nixosModules.stylix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs   = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ankh      = import ./home.nix;
+        }
+      ];
+    };
   };
- };
-} 
+}
--- home.nix
+++ home.nix
@@ -1,9 +1,48 @@
 { config, pkgs, ... }:
 
- {
-  home.username = "ankh";
+{
+  home.username      = "ankh";
   home.homeDirectory = "/home/ankh";
-  home.stateVersion = "25.11";
+  home.stateVersion  = "25.11";
 
   programs.home-manager.enable = true;
- }
+
+  # Wallpaper temporário até termos o matugen
+  # Baixa qualquer imagem e coloca em ~/wallpaper.jpg antes de rodar
+  stylix = {
+    enable   = true;
+    image    = ./wallpaper.jpg;
+    polarity = "dark";
+
+    # Fundos fixos, só acento varia
+    override = {
+      base00 = "0d0d0f";
+      base01 = "111113";
+      base02 = "161618";
+      base03 = "2a2a2e";
+      base04 = "555555";
+      base05 = "cecece";
+      base06 = "e0e0e0";
+      base07 = "f5f5f5";
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name    = "JetBrainsMono Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.inter;
+        name    = "Inter";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name    = "Noto Color Emoji";
+      };
+      sizes = {
+        terminal     = 13;
+        applications = 11;
+      };
+    };
+  };
+}

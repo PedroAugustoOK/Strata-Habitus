3e65134187d330cb8361af1493f3bf1d690191f8
ankh <pedro13marchetti@pm.me>
2026-03-20 15:18:52 -0400

    fix: configuration.nix limpo

--- configuration.nix
+++ configuration.nix
@@ -1,140 +1,105 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [ ./hardware-configuration.nix ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  # Boot
+  boot.loader.systemd-boot.enable      = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  # Rede
+  networking.hostName              = "nixos";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/Porto_Velho";
-
-  # Select internationalisation properties.
+  # Locale
+  time.timeZone      = "America/Porto_Velho";
   i18n.defaultLocale = "pt_BR.UTF-8";
-
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_ADDRESS        = "pt_BR.UTF-8";
     LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
+    LC_MEASUREMENT    = "pt_BR.UTF-8";
+    LC_MONETARY       = "pt_BR.UTF-8";
+    LC_NAME           = "pt_BR.UTF-8";
+    LC_NUMERIC        = "pt_BR.UTF-8";
+    LC_PAPER          = "pt_BR.UTF-8";
+    LC_TELEPHONE      = "pt_BR.UTF-8";
+    LC_TIME           = "pt_BR.UTF-8";
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "br";
-    variant = "";
-  };
-
-  # Configure console keymap
+  # Teclado
+  services.xserver.xkb = { layout = "br"; variant = ""; };
   console.keyMap = "br-abnt2";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Usuário
   users.users.ankh = {
     isNormalUser = true;
-    description = "Pedro Augusto";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
+    description  = "Pedro Augusto";
+    extraGroups  = [ "networkmanager" "wheel" ];
+    packages     = with pkgs; [];
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # Pacotes
   environment.systemPackages = with pkgs; [
     git wget curl neovim
     networkmanager
     kitty
     chromium
-    grimblast
     quickshell
+    grimblast
+    wl-clipboard
+    cliphist
+    brightnessctl
+    swww
+    matugen
+    nautilus
+    gvfs
+    pavucontrol
+    playerctl
+    hyprlock
+    hypridle
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  
-  #Hyprland
-  programs.hyprland.enable = true;
+  # Hyprland
+  programs.hyprland.enable        = true;
   programs.hyprland.xwayland.enable = true;
-  #Portais Wayland
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
-  #login
+
+  # Portais Wayland
+  xdg.portal.enable        = true;
+  xdg.portal.extraPortals  = [ pkgs.xdg-desktop-portal-hyprland ];
+
+  # Login
   services.greetd = {
     enable = true;
     settings.default_session = {
-    command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-    user    = "greeter";
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      user    = "greeter";
     };
   };
 
+  # Fontes
   fonts.packages = with pkgs; [
-  nerd-fonts.jetbrains-mono
-  inter
- ];
+    nerd-fonts.jetbrains-mono
+    inter
+  ];
 
-  system.stateVersion = "25.11"; # Did you read the comment?
-
-  #CLAUDE
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-   hardware.graphics.enable = true;
-   hardware.graphics.extraPackages = with pkgs; [
+  # GPU Intel
+  hardware.graphics.enable        = true;
+  hardware.graphics.extraPackages = with pkgs; [
     intel-media-driver
     intel-vaapi-driver
-   ];
- 
-];
+  ];
 
-stylix = {
-  enable   = true;
-  image    = ./wallpaper.jpg;
-  polarity = "dark";
-};
+  # Nix
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  # Stylix
+  stylix = {
+    enable   = true;
+    image    = ./wallpaper.jpg;
+    polarity = "dark";
+  };
+
+  system.stateVersion = "25.11";
 }

0415a4afe73df80c6a21b9bba60ea03354a2ca2e
ankh <pedro13marchetti@pm.me>
2026-04-17 21:27:29 -0400

    feat: add iso target to flake

--- flake.nix
+++ flake.nix
@@ -11,12 +11,28 @@
   let
     username = "ankh";
     hostname = "nixos";
+    system = "x86_64-linux";
   in {
     nixosConfigurations.galaxybook = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       specialArgs = { inherit username hostname; };
       modules = [
         ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs   = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit username; };
+          home-manager.users.${username} = import ./home.nix;
+        }
+      ];
+    };
+
+    nixosConfigurations.iso = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit username hostname; };
+      modules = [
+        ./iso.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs   = true;

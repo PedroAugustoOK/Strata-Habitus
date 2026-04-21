c19021b4cfa32f7a09cfcc11687dc90d954f240a
Pedro Augusto <pedro13marchetti@pm.me>
2026-04-20 21:37:51 -0400

    chore: theme → rosepine

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -4,6 +4,6 @@ return {
   { "rose-pine/neovim",         name = "rose-pine", lazy = true },
   {
     "LazyVim/LazyVim",
-    opts = { colorscheme = "gruvbox" },
+    opts = { colorscheme = "rose-pine-dawn" },
   },
 }
